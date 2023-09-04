e5697fbda9f9299779e183eb59366cf69ca770a0
Poly A. Titova <poly_titova@mail.ru>
2023-09-04 11:05:18 +0300

    Добавили остальные страницы

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,23 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home.index');
 });
+
+Route::get('/cart', function () {
+    return view('cart.index');
+});
+
+Route::get('/categories', function () {
+    return view('categories.index');
+});
+
+Route::get('/checkout', function () {
+    return view('checkout.index');
+});
+
+Route::get('/contact', function () {
+    return view('contact.index');
+});
+
+Route::get('/product', function () {
+    return view('product.index');
+});
