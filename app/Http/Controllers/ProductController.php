8b191954708c03a1947f388d5ed6abd63a674c17
Poly A. Titova <poly_titova@mail.ru>
2023-09-11 17:26:44 +0300

    Добавили модель и отображение данных из бд

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,12 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
     public function index()
     {
-        return view('product.index');
+        $products = Product::all();
+        foreach ($products as $product) {
+            echo 'Title: ' . $product->title;
+            echo "<br>";
+        }
+
+        // return view('product.index');
     }
 }
