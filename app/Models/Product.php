7a291f056bf6cbb9101bad201f1900d76fda98b6
Poly A. Titova <poly_titova@mail.ru>
2023-09-15 12:15:16 +0300

    Отобразили карточки на главной странице

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,10 +2,11 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    use HasFactory;
+    public function images() {
+        return $this->hasMany('App\Models\ProductImage');
+    }
 }
--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -7,10 +7,10 @@
 
 	<div class="home">
 		<div class="home_slider_container">
-			
+
 			<!-- Home Slider -->
 			<div class="owl-carousel owl-theme home_slider">
-				
+
 				<!-- Slider Item -->
 				<div class="owl-item home_slider_item">
 					<div class="home_slider_background" style="background-image:url(images/home_slider_1.jpg)"></div>
@@ -18,7 +18,7 @@
 						<div class="container">
 							<div class="row">
 								<div class="col">
-									<div class="home_slider_content"  data-animation-in="fadeIn" data-animation-out="animate-out fadeOut">
+									<div class="home_slider_content" data-animation-in="fadeIn" data-animation-out="animate-out fadeOut">
 										<div class="home_slider_title">A new Online Shop experience.</div>
 										<div class="home_slider_subtitle">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam a ultricies metus. Sed nec molestie eros. Sed viverra velit venenatis fermentum luctus.</div>
 										<div class="button button_light home_button"><a href="#">Shop Now</a></div>
@@ -36,7 +36,7 @@
 						<div class="container">
 							<div class="row">
 								<div class="col">
-									<div class="home_slider_content"  data-animation-in="fadeIn" data-animation-out="animate-out fadeOut">
+									<div class="home_slider_content" data-animation-in="fadeIn" data-animation-out="animate-out fadeOut">
 										<div class="home_slider_title">A new Online Shop experience.</div>
 										<div class="home_slider_subtitle">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam a ultricies metus. Sed nec molestie eros. Sed viverra velit venenatis fermentum luctus.</div>
 										<div class="button button_light home_button"><a href="#">Shop Now</a></div>
@@ -54,7 +54,7 @@
 						<div class="container">
 							<div class="row">
 								<div class="col">
-									<div class="home_slider_content"  data-animation-in="fadeIn" data-animation-out="animate-out fadeOut">
+									<div class="home_slider_content" data-animation-in="fadeIn" data-animation-out="animate-out fadeOut">
 										<div class="home_slider_title">A new Online Shop experience.</div>
 										<div class="home_slider_subtitle">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam a ultricies metus. Sed nec molestie eros. Sed viverra velit venenatis fermentum luctus.</div>
 										<div class="button button_light home_button"><a href="#">Shop Now</a></div>
@@ -68,7 +68,7 @@
 			</div>
 
 			<!-- Home Slider Dots -->
-			
+
 			<div class="home_slider_dots_container">
 				<div class="container">
 					<div class="row">
@@ -82,7 +82,7 @@
 							</div>
 						</div>
 					</div>
-				</div>	
+				</div>
 			</div>
 
 		</div>
@@ -129,87 +129,31 @@
 		<div class="container">
 			<div class="row">
 				<div class="col">
-					
+
 					<div class="product_grid">
 
-						<!-- Product -->
-						<div class="product">
-							<div class="product_image"><img src="images/product_1.jpg" alt=""></div>
-							<div class="product_extra product_new"><a href="categories.html">New</a></div>
-							<div class="product_content">
-								<div class="product_title"><a href="product.html">Smart Phone</a></div>
-								<div class="product_price">$670</div>
-							</div>
-						</div>
+						@foreach($products as $product)
+							<!-- Product -->
+							@php
+								$image = '';
+								if (count($product->images) > 0) {
+									$image = $product->images[0]['img'];
+								} else {
+									$image = 'no_image.png';
+								}
+							@endphp
 
-						<!-- Product -->
-						<div class="product">
-							<div class="product_image"><img src="images/product_2.jpg" alt=""></div>
-							<div class="product_extra product_sale"><a href="categories.html">Sale</a></div>
-							<div class="product_content">
-								<div class="product_title"><a href="product.html">Smart Phone</a></div>
-								<div class="product_price">$670</div>
+							<div class="product">
+								<div class="product_image"><img src="images/{{ $image }}" alt="{{ $product->title }}"></div>
+								<div class="product_extra product_new"><a href="categories.html">New</a></div>
+								<div class="product_content">
+									<div class="product_title"><a href="product.html">{{ $product->title }}</a></div>
+									<div class="product_price">${{ $product->price }}</div>
+								</div>
 							</div>
-						</div>
-
-						<!-- Product -->
-						<div class="product">
-							<div class="product_image"><img src="images/product_3.jpg" alt=""></div>
-							<div class="product_content">
-								<div class="product_title"><a href="product.html">Smart Phone</a></div>
-								<div class="product_price">$670</div>
-							</div>
-						</div>
-
-						<!-- Product -->
-						<div class="product">
-							<div class="product_image"><img src="images/product_4.jpg" alt=""></div>
-							<div class="product_content">
-								<div class="product_title"><a href="product.html">Smart Phone</a></div>
-								<div class="product_price">$670</div>
-							</div>
-						</div>
-
-						<!-- Product -->
-						<div class="product">
-							<div class="product_image"><img src="images/product_5.jpg" alt=""></div>
-							<div class="product_content">
-								<div class="product_title"><a href="product.html">Smart Phone</a></div>
-								<div class="product_price">$670</div>
-							</div>
-						</div>
-
-						<!-- Product -->
-						<div class="product">
-							<div class="product_image"><img src="images/product_6.jpg" alt=""></div>
-							<div class="product_extra product_hot"><a href="categories.html">Hot</a></div>
-							<div class="product_content">
-								<div class="product_title"><a href="product.html">Smart Phone</a></div>
-								<div class="product_price">$670</div>
-							</div>
-						</div>
-
-						<!-- Product -->
-						<div class="product">
-							<div class="product_image"><img src="images/product_7.jpg" alt=""></div>
-							<div class="product_content">
-								<div class="product_title"><a href="product.html">Smart Phone</a></div>
-								<div class="product_price">$670</div>
-							</div>
-						</div>
-
-						<!-- Product -->
-						<div class="product">
-							<div class="product_image"><img src="images/product_8.jpg" alt=""></div>
-							<div class="product_extra product_sale"><a href="categories.html">Hot</a></div>
-							<div class="product_content">
-								<div class="product_title"><a href="product.html">Smart Phone</a></div>
-								<div class="product_price">$670</div>
-							</div>
-						</div>
+						@endforeach
 
 					</div>
-						
 				</div>
 			</div>
 		</div>
@@ -239,7 +183,7 @@
 	<div class="icon_boxes">
 		<div class="container">
 			<div class="row icon_box_row">
-				
+
 				<!-- Icon Box -->
 				<div class="col-lg-4 icon_box_col">
 					<div class="icon_box">
@@ -290,7 +234,9 @@
 				<div class="col-lg-8 offset-lg-2">
 					<div class="newsletter_content text-center">
 						<div class="newsletter_title">Subscribe to our newsletter</div>
-						<div class="newsletter_text"><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam a ultricies metus. Sed nec molestie eros</p></div>
+						<div class="newsletter_text">
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam a ultricies metus. Sed nec molestie eros</p>
+						</div>
 						<div class="newsletter_form_container">
 							<form action="#" id="newsletter_form" class="newsletter_form">
 								<input type="email" class="newsletter_input" required="required">
